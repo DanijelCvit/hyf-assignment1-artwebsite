5b890f4c6cabbca4559f29e484d19f70fd1b2556
HalfdanWhiteshirt <67345575+HalfdanWhiteshirt@users.noreply.github.com>
2021-07-27 21:32:50 +0200

    Add files via upload

--- code.js
+++ code.js
@@ -1,16 +1,71 @@
+let galleryHash = "";
+
 /* Remove target # on page refresh */
 window.onload = function(){
     document.location.hash = "";
 }
 
+function setHash(hashValue) {
+    galleryHash = hashValue;
+} 
+
 /* Set modal source to clicked image and set close button target to originating gallery */
 function setModalSource(event){
     image.src = event.target.src;
-
-    let hash = image.src.split(`#`)[1];
-
-    cbutton.setAttribute("href", `#${hash}`);
-
 }
+
+function closeModalView() {
+    document.location.hash = galleryHash;
+}
+
+/* Go to previous image in the gallery */
+function prevModalImage(){
+
+   let galleryID = galleryHash;
+   let galleryItems = document.body.querySelectorAll(`#${galleryID} .gallery-item`);
+   let modalImage = modal.querySelector(`img`);
+   let currentImageIndex = 0;
+
+   for (let i = 0; i < galleryItems.length; i++) {
+
+    if (galleryItems[i].querySelector(`img`).src == modalImage.src) {
+        currentImageIndex = i;
+        break;
+    }
+   }
+
+   if (currentImageIndex == 0 ) {
+       modalImage.src = galleryItems[galleryItems.length-1].querySelector(`img`).src;
+   }
+   else {
+       modalImage.src = galleryItems[currentImageIndex-1].querySelector(`img`).src; 
+   }
+}
+
+/* Go to the next image in the gallery */
+function nextModalImage(){
+
+    let galleryID = galleryHash;
+    let galleryItems = document.body.querySelectorAll(`#${galleryID} .gallery-item`);
+    let modalImage = modal.querySelector(`img`);
+    let currentImageIndex = 0;
+ 
+    for (let i = 0; i < galleryItems.length; i++) {
+ 
+     if (galleryItems[i].querySelector(`img`).src == modalImage.src) {
+         currentImageIndex = i;
+         break;
+     }
+    }
+ 
+    if (currentImageIndex == (galleryItems.length-1) ) {
+        modalImage.src = galleryItems[0].querySelector(`img`).src;
+    }
+    else {
+        modalImage.src = galleryItems[currentImageIndex+1].querySelector(`img`).src; 
+    } 
+}
+
+
 
 
